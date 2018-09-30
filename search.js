323a86c01a218a3cf53d37e76bab9356a162b966
dmk1111 <dima.golysh@gmail.com>
2018-09-30 22:15:30 +0300

    add asynchronous search with read streams

--- search.js
+++ search.js
@@ -10,9 +10,9 @@ function searchSync(dir, searchString, fileList = []) {
         catch (err) {
             if (err.code === 'ENOTDIR' || err.code === 'EBUSY') {
                 const data = fs.readFileSync(dirFile);
-                    if(data.indexOf(searchString) >= 0){
-                        fileList = [...fileList, dirFile];
-                    }
+                if (data.indexOf(searchString) >= 0) {
+                    fileList = [...fileList, dirFile];
+                }
             } else {
                 throw err;
             }
@@ -22,3 +22,43 @@ function searchSync(dir, searchString, fileList = []) {
 }
 
 console.log(searchSync('./test', 'Lorem'));
+
+function searchAsync(dir, searchString, done) {
+    let results = [];
+    fs.readdir(dir, function (err, list) {
+        if (err) return done(err);
+        let pending = list.length;
+        if (!pending) return done(null, results);
+        list.forEach(function (file) {
+            file = path.join(dir, file);
+            fs.stat(file, function (err, stat) {
+                if (stat && stat.isDirectory()) {
+                    searchAsync(file, searchString, function (err, res) {
+                        results = [...results, ...res];
+                        if (!--pending) {
+                            done(null, results);
+                        }
+                    });
+                } else {
+                    const stream = fs.createReadStream(file);
+                    stream.on('data', (chunk) => {
+                        if (chunk.indexOf(searchString) >= 0) {
+                            results.push(file);
+                            stream.close();
+                        }
+                    });
+                    stream.on('close', () => {
+                        if (!--pending) {
+                            done(null, results);
+                        }
+                    });
+                }
+            });
+        });
+    });
+}
+
+searchAsync('./test', 'Lorem', function(err, results) {
+    if (err) throw err;
+    console.log(results);
+});
